eb94f8044ffa90694d24a75a3f24fbcf5d49ff1b
gauri gattani <gauri.gattani@massive.co>
2018-12-17 09:36:26 -0500

    React Redux app creted using 2 tutorials - react, redux from egghead

--- src/index.js
+++ src/index.js
@@ -1,12 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore} from 'redux';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//state = 0 - default state
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state;
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(counter)
+
+store.subscribe(() => {
+	ReactDOM.render(
+	<App 
+		val={store.getState()}
+      	onIncrement={() => 
+        store.dispatch({ type: 'INCREMENT' })
+      	}
+      	onDecrement={() => 
+        store.dispatch({ type: 'DECREMENT' })
+      }
+    />, 
+	document.getElementById('root')
+)});
+
+store.dispatch({type : ''})
+//to ensure default state is firing subscribe event
+
+
+/*
+ALL IN ONE CLASS
+import React from 'react';
+import {createStore} from 'redux';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super()
+  }
+  render(){
+    return (
+      <div>
+      <h1>{this.props.val}</h1>
+      <button onClick={this.props.onIncrement}>+</button>
+      <button onClick={this.props.onDecrement}>-</button>
+      </div> )
+  }
+}
+
+
+
+const render = () => {
+  ReactDOM.render(
+    <App 
+      val={store.getState()}
+      onIncrement={() => 
+        store.dispatch({
+          type: 'INCREMENT'
+        })
+      }
+      onDecrement={() => 
+        store.dispatch({
+          type: 'DECREMENT'
+        })
+      }/>, 
+    document.getElementById('root'))
+}
+store.subscribe((render))
+
+render()
+*/
+
+
